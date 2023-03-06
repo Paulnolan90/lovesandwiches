37617637fcf24aec9c77c761e76574b32b53b7e2
Paulnolan90 <paulnolan1990@gmail.com>
2023-03-06 18:55:09 +0000

    gety latest stock nums

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -64,6 +65,24 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("sales worksheet update success.\n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+
+    print("Calculating surplus...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    run all program functions 
+    """
+
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to love Sanwiches Data Automation")
+main()
